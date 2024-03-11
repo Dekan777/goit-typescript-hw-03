60332999fa52a80152301b132959bdac2bac46f3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-11 12:14:42 +0100

    class Kay

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,24 @@
-const key = new Key();
+// Ключ (Key): Створіть клас Key. У нього має бути одна приватна
+// властивість signature, яка генерується випадково при створенні
+// об'єкта цього класу (наприклад Math.random()). Також цей клас
+// повинен мати метод getSignature, який повертає значення властивості
+//  signature.
+class Kay {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
