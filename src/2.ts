396a1fd194a9f5d2ca5dbf6dffafb3a357d4749e
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-11 11:54:57 +0100

    2 prot

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -34,6 +34,5 @@ class Employee {
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
