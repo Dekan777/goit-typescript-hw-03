0ba69774cd177aed32efa29e6d85d2b111cadedf
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-11 12:24:31 +0100

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -15,6 +15,69 @@ class Kay {
   }
 }
 
+// Людина (Person): Створіть клас Person. Конструктор цього
+// класу приймає об'єкт класу Key і зберігає їх у приватному
+//  властивості key. Клас Person повинен мати метод getKey,
+//   який повертає збережений ключ.
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Дім (House): Створіть абстрактний клас House. Цей клас має дві
+//  властивості: door, яка може бути відкрита (true), або
+//   закрита (false), і key, яка зберігає об'єкт класу Key.
+//   У цьому класі також повинен бути метод comeIn, який
+//   додає об'єкт класу Person у масив tenants, якщо door відкрита.
+//    Ваш абстрактний клас House також повинен мати абстрактний
+//    метод OpenDoor, який приймає об'єкт класу Key.
+abstract class House {
+  public door: boolean = false;
+  private key: Key;
+
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.constructor.name} entered the house`);
+    } else {
+      console.log("Door is closed");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується
+// від абстрактного класу House. Реалізуйте метод openDoor у цьому
+// класі. Якщо ключ, переданий цьому методу, збігається з ключем,
+// збереженим як key, то двері відчиняються.
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door opened");
+    } else {
+      console.log("Wrong key");
+    }
+  }
+}
+
 const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
